cb0a88c4be532902238f6584ca93fc8492b98749
Valentin Dancheff <dancheff.valentin@gmail.com>
2021-03-21 23:20:49 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,6 +7,8 @@ echo " / / /_/ / / / / / / /_/ /  (__  ) /_/ /_/ / /__/ ,<    / / / / (__  ) /_/
 echo "/_/\__,_/_/ /_/ /_/ .___/  /____/\__/\__,_/\___/_/|_|  /_/_/ /_/____/\__/\__,_/_/_/   "
 echo "                 /_/                                                                  "
 
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
+export PATH
 
 if [[ "$EUID" -ne 0 ]]; then
   echo -e '\nERROR!!! SCRIPT MUST RUN WITH ROOT PRIVILEGES\n'
